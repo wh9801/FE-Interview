e85598a197b8680c3aa0799611161b2a7a0b9515
wh9801 <philwu98@163.com>
2021-11-03 14:46:02 +0800

    feat: 二分查找

---
+++ Algorithm/二分查找.js
@@ -0,0 +1,19 @@
+let search = function (nums, target) {
+    let right = nums.length - 1;
+    let left = 0;
+    while (left <= right) {
+        let mid = Math.floor((right + left) / 2);
+        console.log(mid);
+        if (nums[mid] > target) {
+            right = mid - 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+}
+
+
+console.log(search([-1, 0, 3, 5, 9, 12], 2));

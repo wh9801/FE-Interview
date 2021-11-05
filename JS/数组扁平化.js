3134ed429d3b9004fbc6771a056c2af48f89f139
wh9801 <philwu98@163.com>
2021-11-05 15:56:54 +0800

    feat: 数组扁平化

--- JS/数组扁平化.js
+++ JS/数组扁平化.js
@@ -1,19 +1,22 @@
 // 元素为数组时递归调用
-function flatten1(arr){
-    if(!Array.isArray(arr))
+function flatten1(arr) {
+    if (!Array.isArray(arr))
         return;
-    let res=[];
-    res=arr.reduce((pre,cur)=>{
+    let res = [];
+    res = arr.reduce((pre, cur) => {
         return pre.concat(Array.isArray(cur) ? flatten1(cur) : cur);
-    },[]);
+    }, []);
     return res;
 }
 
 // 使用Array.toString()
-function flatten2(arr){
-    return arr.toString().split(",").map((item)=>+item);
+function flatten2(arr) {
+    return arr.toString().split(",").map(item =>  +item);
 }
 
-console.log([[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]].toString().split(","));
-console.log(flatten1([[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]));
+// 使用flat
+console.log([[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]].flat());
+
+console.log(flatten2([[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]));
+console.log(flatten1([[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]));
 

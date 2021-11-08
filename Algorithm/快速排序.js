fc051358755b4222ae399690b039b7a1b5b68293
wh9801 <philwu98@163.com>
2021-11-08 16:43:06 +0800

    feat

--- Algorithm/快速排序.js
+++ Algorithm/快速排序.js
@@ -1,15 +1,21 @@
+// 使用填坑法，一次划分后，有一个元素的最终位置确定了
 function partition(arr, start, end) {
+    // 以第一个元素作为划分值
     let t = arr[start];
     while (start < end) {
+        // 向左遍历，找到比划分值小的，填入坑中
         while (arr[end] >= t && start < end) {
             end--;
         }
         arr[start] = arr[end];
+
+        // 向右遍历，找到比划分值大的，填入坑中
         while (arr[start] <= t && start < end) {
             start++;
         }
         arr[end] = arr[start];
     }
+    // 将划分值填入正确的位置
     arr[start] = t;
     return start;
 }
